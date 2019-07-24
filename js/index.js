65528c7718a71737191c81cac4339dded75c022b
liushuai <liushuai@teddymobile.cn>
2019-07-24 18:33:24 +0800

    zoom

--- js/index.js
+++ js/index.js
@@ -24,10 +24,66 @@ $(function () {
         }
     }
 
+    // 新闻资讯
+    var newsCenter = {
+        render(news) {
+            var _this = this
+            var newsStr = ''
+            if(news) {
+                news.forEach(function(item, index) {
+                    newsStr += _this.createItem(item)
+                })
+                $('.news-center__list').html(newsStr)
+            }
+        },
+        createItem(item, index) {
+            return '<li class="news-center__list-item fl">\n' +
+                '                                <div class="news-center__list-item__title">\n' +
+                '                                    <a href="news-detail.html">'+item.title+'</a>\n' +
+                '                                </div>\n' +
+                '                                <div class="news-center__list-item__content">\n' +
+                                                    item.content +
+                '                                </div>\n' +
+                '                                <div class="detail-btn fr">\n' +
+                '                                    <a href="news-detail.html">查看详情</a>\n' +
+                '                                </div>\n' +
+                '                            </li>'
+        }
+    }
+
+    // 作品欣赏
+    var productRender = {
+        render(products) {
+            var _this = this
+            var newsStr = ''
+            if(products) {
+                products.forEach(function(item, index) {
+                    newsStr += _this.createItem(item)
+                })
+                $('.product__list').html(newsStr)
+            }
+        },
+        createItem(item, index) {
+            return '<li class="product__list-item">\n' +
+                '                                <a href="./product-detail.html" class="product__list-item-link">\n' +
+                '                                    <div class="product__list-item__img-wrap">\n' +
+                '                                        <img src='+item.image+' alt="描述信息">\n' +
+                '                                    </div>\n' +
+                '                                    <div class="product__list-item__txt">\n' +
+                '                                        <div class="txt cleaarfix">\n' +
+                '                                            <span>'+item.type+'</span>\n' +
+                '                                            <i class="icon icon-link fr"></i>\n' +
+                '                                        </div>\n' +
+                '                                    </div>\n' +
+                '                                </a>\n' +
+                '                            </li>'
+        }
+    }
 
 
     $.get('mock/index.json', null, function (data) {
-        var news = data.news
-        newsRender.render(news)
+        newsRender.render(data.news)
+        newsCenter.render(data['news-center'])
+        productRender.render(data.products)
     })
 })
