9aebcdc79a2a62d7257a59c8772e5b1b2d6d49ab
liushuai121998 <liushuai1219983341@163.com>
2019-07-30 23:40:32 +0800

    调整样式

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-$(function () {
+$(function() {
     //    核心业务
     var newsRender = {
         render(news) {
             var _this = this
             var newsStr = ''
-            if(news) {
+            if (news) {
                 news.forEach(function(item, index) {
                     newsStr += _this.createItem(item, index)
                 })
@@ -14,11 +14,11 @@ $(function () {
         createItem(item, index) {
             return '<li class="core_news__list-item fl">\n' +
                 '                                <div class="core_news__list-item__img">\n' +
-                '                                    <img src='+item.image+' alt="">\n' +
+                '                                    <img src=' + item.image + ' alt="">\n' +
                 '                                </div>\n' +
                 '                                <div class="core-news__list-item__info">\n' +
-                '                                    <p>0'+(index+1)+'/</p>\n' +
-                '                                    <p>'+item.title+'</p>\n' +
+                '                                    <p>0' + (index + 1) + '/</p>\n' +
+                '                                    <p>' + item.title + '</p>\n' +
                 '                                </div>\n' +
                 '                            </li>'
         }
@@ -29,7 +29,7 @@ $(function () {
         render(news) {
             var _this = this
             var newsStr = ''
-            if(news) {
+            if (news) {
                 news.forEach(function(item, index) {
                     newsStr += _this.createItem(item)
                 })
@@ -37,15 +37,15 @@ $(function () {
             }
         },
         createItem(item, index) {
-            return '<li class="news-center__list-item fl">\n' +
+            return '<li class="news-center__list-item fl col-md-4 col-sm-6 col-xs-12">\n' +
                 '                                <div class="news-center__list-item__title">\n' +
-                '                                    <a href="contacttail.html">'+item.title+'</a>\n' +
+                '                                    <a href="javascript:;">' + item.title + '</a>\n' +
                 '                                </div>\n' +
                 '                                <div class="news-center__list-item__content">\n' +
-                                                    item.content +
+                item.content +
                 '                                </div>\n' +
                 '                                <div class="detail-btn fr">\n' +
-                '                                    <a href="contacttail.html">查看详情</a>\n' +
+                '                                    <a href="javascript:;">查看详情</a>\n' +
                 '                                </div>\n' +
                 '                            </li>'
         }
@@ -56,7 +56,7 @@ $(function () {
         render(products) {
             var _this = this
             var newsStr = ''
-            if(products) {
+            if (products) {
                 products.forEach(function(item, index) {
                     newsStr += _this.createItem(item)
                 })
@@ -64,14 +64,14 @@ $(function () {
             }
         },
         createItem(item, index) {
-            return '<li class="product__list-item">\n' +
+            return '<li class="product__list-item col-md-4 col-sm-6 col-xs-12">\n' +
                 '                                <a href="./product-detail.html" class="product__list-item-link">\n' +
                 '                                    <div class="product__list-item__img-wrap">\n' +
-                '                                        <img src='+item.image+' alt="描述信息">\n' +
+                '                                        <img src=' + item.image + ' alt="描述信息">\n' +
                 '                                    </div>\n' +
                 '                                    <div class="product__list-item__txt">\n' +
                 '                                        <div class="txt cleaarfix">\n' +
-                '                                            <span>'+item.type+'</span>\n' +
+                '                                            <span>' + item.type + '</span>\n' +
                 '                                            <i class="icon icon-link fr"></i>\n' +
                 '                                        </div>\n' +
                 '                                    </div>\n' +
@@ -81,7 +81,7 @@ $(function () {
     }
 
 
-    $.get('mock/index.json', null, function (data) {
+    $.get('mock/index.json', null, function(data) {
         newsRender.render(data.news)
         newsCenter.render(data['news-center'])
         productRender.render(data.products)
